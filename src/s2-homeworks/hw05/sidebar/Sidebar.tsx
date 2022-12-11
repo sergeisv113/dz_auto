73560cf096e74d5bcc3e23d68ed5603f8088ac0d
Sergey <sergeisv113@gmail.com>
2022-12-11 18:08:20 +0200

    commit 5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -10,8 +10,10 @@ type PropsType = {
 }
 
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
-    const sidebarClass = s.sidebar
-        + (open ? ' ' + s.open : '')
+    const openClass = open ? ' ' + s.open : ''
+    const sidebarClass = `${s.sidebar} ${openClass}` // navlink class
+    const navClass = open ? s.nav : s.navHidden //nav class
+
     return (
         <>
             {/*затемнение справа от открытого меню*/}
